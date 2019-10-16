a2bf3013482fa319ecef3fabf0b7e3425ba1df0e
7nohe <7nohe.urata@gmail.com>
2019-10-16 11:31:41 +0900

    add camera permission

--- app/CameraScreen/CameraScreen.tsx
+++ app/CameraScreen/CameraScreen.tsx
@@ -2,10 +2,24 @@ import React, { useState, useEffect } from "react";
 import { Button, Image, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import Layout from "../Layout";
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+
+
+const getPermissionAsync = async () => {
+  if (Constants.platform.ios) {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.CAMERA);
+    if (status !== 'granted') {
+      alert('Sorry, we need camera roll permissions to make this work!');
+    }
+  }
+}
 
 const CameraScreen = () => {
   const [photo, setPhoto] = useState(null);
-
+  useEffect(() => {
+    getPermissionAsync();
+  }, [])
   const _pickImage = async () => {
     let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
